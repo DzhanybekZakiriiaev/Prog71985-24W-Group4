e7f4aced629ffece48fe1aaed7bd920e1740f6a2
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2024-04-02 21:28:28 -0400

    added search template, dispose tasklist and file read/write

--- Prog71985-24W-Group4/task.c
+++ Prog71985-24W-Group4/task.c
@@ -4,13 +4,12 @@
 #include <stdio.h>
 #include <string.h> //for strcpy and memcpy
 
-TASK CreateTask(char name[MAXNAME], int priorityLevel, char content[MAXCONTENT]) {// , STATE state, time_t time) {
+TASK CreateTask(char name[MAXNAME], int priorityLevel, char content[MAXCONTENT], STATE state) {//, time_t time) {
 	TASK t;
-	t.isEmpty = false;
 	strcpy(t.name, name);
 	t.priorityLevel = priorityLevel;
 	strcpy(t.content, content);
-	//t.state = state;
+	t.state = state;
 	//t.time = time;
 	return t;
 }
@@ -33,39 +32,5 @@ bool CompareTask(TASK lhs, TASK rhs) {
 }
 
 void DisposeTask(TASK t) {
-	// nothing to be done yet? 
+	// nothing
 }
-
-
-//-------KEEPING THESE FUNCTIONS HERE FOR NOW UNTIL WE MOVE THEM-------//
-//void addTaskToList(TASK taskList[], char name[MAXNAME], int priorityLevel, char content[MAXCONTENT]) {// , STATE state, time_t time) {
-//	TASK newTask = createTask(name, priorityLevel, content);// , state, time);
-//	for (int index = 0; index < MAX_TASKS; index++) {
-//		if (taskList[index].isEmpty) {
-//			taskList[index] = newTask;
-//			printf("index:%d\ntask: %s\n", index, taskList[index].name);//for debugging
-//			return;
-//		}
-//	}
-//}
-//
-//void deleteTaskFromList(TASK taskList[], char taskToDelete[MAXNAME]) {
-//	for (int index = 0; index < MAX_TASKS; index++) {
-//		if (strcmp(taskList[index].name, taskToDelete)) { //finds the correct task to delete by comparing names
-//			taskList[index].isEmpty = 1;
-//			strcpy(taskList[index].name, "");//set all values to empty
-//			taskList[index].priorityLevel = 0;
-//			strcpy(taskList[index].content, "");
-//		}
-//	}
-//}
-//
-//void updateTaskFromList(TASK taskList[], char taskToUpdate[MAXNAME], char newName[MAXNAME], int newPriorityLevel, char newContent[MAXCONTENT]) {
-//	for (int index = 0; index < MAX_TASKS; index++) {
-//		if (strcmp(taskList[index].name, taskToUpdate) == 0) { //finds the correct task to update by comparing names
-//			strcpy(taskList[index].name, newName);//sets to new values as specified by user
-//			taskList[index].priorityLevel = newPriorityLevel;
-//			strcpy(taskList[index].content, newContent);
-//		}
-//	}
-//}
--- Prog71985-24W-Group4/task.h
+++ Prog71985-24W-Group4/task.h
@@ -4,18 +4,16 @@
 #include <stdbool.h>
 #define MAXNAME 40
 #define MAXCONTENT 300
-#define MAX_TASKS 25 //maximum number of tasks in the tasklist
 
 typedef struct task {
-	bool isEmpty; //to keep track of which element in the array it is
 	char name[MAXNAME];
 	int priorityLevel;
 	char content[MAXCONTENT];
-	//STATE state; //commented out until these are built
+	STATE state;
 	//time_t time;
 }TASK;
 
-TASK CreateTask(char name[MAXNAME], int priorityLevel, char content[MAXCONTENT]);//, STATE state, time_t time);
+TASK CreateTask(char name[MAXNAME], int priorityLevel, char content[MAXCONTENT], STATE state);//, STATE state, time_t time);
 
 TASK CopyTask(TASK src);
 
@@ -24,11 +22,3 @@ bool CompareTask(TASK lhs, TASK rhs);
 void PrintTask(TASK t);
 
 void DisposeTask(TASK t);
-
-
-//-------KEEPING THESE FUNCTIONS HERE FOR NOW UNTIL WE MOVE THEM-------//
-//void addTaskToList(TASK taskList[], char name[MAXNAME], int priorityLevel, char content[MAXCONTENT]);// , STATE state, time_t time);
-//
-//void deleteTaskFromList(TASK taskList[], char taskToDelete[MAXNAME]);
-//
-//void updateTaskFromList(TASK taskList[], char taskToUpdate[MAXNAME], char newName[MAXNAME], int newPriorityLevel, char newContent[MAXCONTENT]);
